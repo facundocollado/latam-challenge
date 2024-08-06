650b781f8cd08708584546f9d0e24de2d7d522e6
facundocollado <colladofacundo@hotmail.com>
2024-08-06 15:02:03 -0300

    Added aasync functions for increased performance

--- challenge/api.py
+++ challenge/api.py
@@ -2,12 +2,31 @@ import os
 import pickle
 from typing import Any, Dict, List
 import fastapi
-import numpy as np
 import pandas as pd
+import aiofiles
 from challenge.model import DelayModel
 from pydantic import BaseModel
 
 app = fastapi.FastAPI()
+
+model = None
+
+# Load the model asynchronously
+async def load_model():
+    model_path = os.path.abspath("data/delay_model.pkl")
+    async with aiofiles.open(model_path, 'rb') as file:
+        model_data = await file.read()
+    
+    model = DelayModel()
+    model._model = pickle.loads(model_data)
+    
+
+    return model
+
+@app.on_event("startup")
+async def startup_event():
+    global model
+    model = await load_model()
 
 
 @app.get("/health", status_code=200)
@@ -21,27 +40,12 @@ class Flight(BaseModel):
     TIPOVUELO: str
     MES: int
 
-    # @field_validator('MES')
-    # def validate_mes(cls, value):
-    #     if not (1 <= value <= 12):
-    #         raise ValueError('MES must be between 1 and 12')
-    #     return value
     @property
     def is_valid_mes(self) -> bool:
         return 1 <= self.MES <= 12
 
 class PredictionRequest(BaseModel):
     flights: List[Flight]
-
-model = None
-
-def load_model():
-    global model
-    if model is None:
-        model_path = os.path.abspath("data/delay_model.pkl")
-        model = DelayModel()
-        with open(model_path, 'rb') as file:
-            model._model = pickle.load(file)
 
 
 @app.post("/predict", status_code=200)
@@ -75,40 +79,14 @@ async def post_predict(request: PredictionRequest) -> Dict[str, Any]:
     # Initialize DataFrame with the necessary columns
     df = df.reindex(columns=FEATURES_COLS, fill_value=0)
     
-    predictions = model.predict(features=df)
+    predictions = []
+
+    try:
+        predictions = model.predict(features=df)
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        raise fastapi.HTTPException(status_code=500, detail="Internal Server Error")
+                                    
+
     return {"predict": predictions}
-
-    # # Convert the values in data["flights"] into a key_value list
-    # key_value_list = []
-    # for flight in request.flights:
-    #     if not flight.is_valid_mes:
-    #         raise fastapi.HTTPException(status_code=400, detail="MES must be between 1 and 12")
-        
-    #     for key, value in flight.model_dump().items():
-    #         key_value_list.append(f"{key}_{value}")
-
-    # # Initialize the DataFrame with FEATURES_COLS, setting all values to False
-    # df = pd.DataFrame(columns=FEATURES_COLS)
-    # row = {col: False for col in FEATURES_COLS}
-
-    # # Update the row based on key_value_list
-    # for kv in key_value_list:
-    #     if kv in FEATURES_COLS:
-    #         row[kv] = True  # Set to 1 or any desired value
-
-    # # Append the row to the DataFrame
-    # row_df = pd.DataFrame([row])
-    # df = pd.concat([df, row_df], ignore_index=True)
-
-    # model_path = os.path.abspath("data/delay_model.pkl")
-
-    # model = DelayModel()
-    # with open(model_path, 'rb') as file:
-    #         model._model = pickle.load(file)
-
-    # prediction = model.predict(
-    #     features=df
-    # )
-
-
-    # return {"predict": prediction}
